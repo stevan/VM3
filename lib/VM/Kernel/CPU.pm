b7d920565f4dd2978daabb0eb1caae94343a409f
Stevan Little <stevan.little@gmail.com>
2024-09-03 18:45:12 +0200

    working ish

--- lib/VM/Kernel/CPU.pm
+++ lib/VM/Kernel/CPU.pm
@@ -7,20 +7,30 @@ class VM::Kernel::CPU {
     use constant DEBUG => $ENV{DEBUG} // 0;
     use overload '""' => \&to_string;
 
-    field $code;
-    field $context;
+    field $code      :reader;
+    field $microcode :reader;
+    field $context   :reader;
 
     field $ic :reader = 0;
     field $ci :reader = undef;
 
-    method load_code ($c) {
-        $code = $c;
-        $ic   = 0;
-        $ci   = undef;
+    method reset {
+        $ic = 0;
+        $ci = undef;
         $self;
     }
 
-    method load_context ($ctx) {
-        $context = $ctx;
+    method load_microcode ($mc)  { $microcode = $mc;  $self }
+    method load_code      ($c)   { $code      = $c;   $self }
+    method load_context   ($ctx) { $context   = $ctx; $self }
+
+    method next_op { $code->[ $context->pc++ ] }
+
+    method execute {
+        $ci = $context->pc;
+        return if $ci < scalar @$code;
+        my $opcode = $self->next_op;
+        $microcode[ $opcode ]->( $self );
+        return ++$ic;
     }
 }
